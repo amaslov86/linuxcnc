834e925a6e5e1a107f22baa30c56d2a210b1d626
notroot <notroot@LCNC>
2021-09-15 09:38:12 +0300

    Все изменения на 15 сентября 2021 года

---
+++ craftex_launcher.sh
@@ -0,0 +1,16 @@
+. /home/notroot/linuxcnc-dev/GLOBAL_CRAFTEX.config
+if [ $CRAFTEX_AXIS_COUNT -eq 3 ]; then
+echo "AXIS COUNT IS 3"
+cp /home/notroot/linuxcnc-dev/bin/craftex/gmoccapy3 /home/notroot/linuxcnc-dev/bin/gmoccapy
+cp /home/notroot/linuxcnc-dev/share/gmoccapy/craftex/gmoccapy3.glade /home/notroot/linuxcnc-dev/share/gmoccapy/gmoccapy.glade
+cd ~/linuxcnc-dev/scripts/
+nice -20 ./linuxcnc ~/linuxcnc/configs/LCNC3/LCNC.ini
+elif [ $CRAFTEX_AXIS_COUNT -eq 4 ]; then
+echo "AXIS COUNT IS 4"
+cp /home/notroot/linuxcnc-dev/bin/craftex/gmoccapy4 /home/notroot/linuxcnc-dev/bin/gmoccapy
+cp /home/notroot/linuxcnc-dev/share/gmoccapy/craftex/gmoccapy4.glade /home/notroot/linuxcnc-dev/share/gmoccapy/gmoccapy.glade
+cd ~/linuxcnc-dev/scripts/
+nice -20 ./linuxcnc ~/linuxcnc/configs/LCNC4/LCNC.ini
+else
+echo "Error in config file"
+fi
